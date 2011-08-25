bb4cc01b91982c02d998f0c3fff96dc38854b887
Martin Lukac <martin@nexleaf.org>
2011-08-24 22:10:14 -0700

    replaced file listing with grid of audio files and log files per device per day

--- seabird/deployed/NexleafGatewayWeb/src/SeabirdGatewayPortal/views/files.py
+++ seabird/deployed/NexleafGatewayWeb/src/SeabirdGatewayPortal/views/files.py
@@ -58,3 +58,63 @@ def view_incoming(request):
     c = RequestContext(request, {'incout': incout, 'totalfiles': len(sfiles)})
     return HttpResponse(t.render(c))
 
+@login_required
+def view_incoming_grid(request):
+    
+    indir = settings.INCOMING_DIR
+    filelist = os.listdir(indir)
+    
+    allsta = []
+    alldates = []
+    for f in filelist:
+        if not f.endswith("zip"):
+            continue
+
+        #SEFI2011_A1000017EBA720_20110824_010015.zip
+	fsp = f.split('_')
+        fdate = fsp[2]
+        fsta = fsp[1]
+        if fdate not in alldates:
+            alldates.append(fdate)
+        if fsta not in allsta:
+            allsta.append(fsta)
+
+    alldates.sort()
+
+    datelookup = {}
+    for i in range(len(alldates)):
+        datelookup[alldates[i]] = i
+
+    alldata = {}
+
+    totaldata = 0
+
+    for sta in allsta:
+        dd = []
+        for dt in alldates:
+            dd.append({'date': dt, 'audio': 0, 'logs': 0})
+        alldata[sta] = {'sta': sta, 'daydata': dd}
+
+
+    for f in filelist:
+        if not f.endswith("zip"):
+            continue
+
+	fsp = f.split('_')
+        fdate = fsp[2]
+        fsta = fsp[1]
+        s = os.stat(indir + f)
+
+        totaldata += s.st_size
+
+        if s.st_size > 150000:
+            alldata[fsta]['daydata'][datelookup[fdate]]['audio'] += 1
+        else:
+            alldata[fsta]['daydata'][datelookup[fdate]]['logs'] += 1
+        
+    t = loader.get_template('files_grid.html')
+    c = RequestContext(request, {'dates': alldates, 'devdata': alldata.values(), 'totalfiles': len(filelist), 'totaldata': totaldata})
+    return HttpResponse(t.render(c))
+
+
+            
